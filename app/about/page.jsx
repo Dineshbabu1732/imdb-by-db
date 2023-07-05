4fe00b11c8f79e8d02cbeb2ba36db41adbab4621
dineshbabu <dineshbabu1732@gmail.com>
2023-07-06 00:19:45 +0530

    create about page

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quia temporibus recusandae. Ducimus expedita totam natus quam placeat nostrum possimus atque, reprehenderit eligendi pariatur, quae ab incidunt aut quasi ipsa aliquam commodi adipisci numquam fugit! Ea accusantium, eveniet quidem ad nisi deleniti provident id natus reprehenderit minima eaque fuga veniam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quia temporibus recusandae. Ducimus expedita totam natus quam placeat nostrum possimus atque, reprehenderit eligendi pariatur, quae ab incidunt aut quasi ipsa aliquam commodi adipisci numquam fugit! Ea accusantium, eveniet quidem ad nisi deleniti provident id natus reprehenderit minima eaque fuga veniam.</p>
+    </div>
+  );
+}
