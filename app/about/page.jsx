e3fd65e7bec161605c428dae256a15f99ce38869
dineshbabu <dineshbabu1732@gmail.com>
2023-07-07 17:34:41 +0530

    add loading

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About
+() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 mt-6'>
+<h1 className='text-2xl font-medium text-amber-500'>About</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sapiente quasi architecto sit? Adipisci minus eveniet architecto nulla voluptatem suscipit dolores, quos voluptas dolor iure, explicabo ipsa! Sequi accusantium nostrum excepturi inventore rerum, possimus modi quo aperiam saepe iure natus fugiat qui neque dolorum! Voluptatem odit unde omnis corporis necessitatibus.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam recusandae deserunt ipsam ea, quos nobis commodi, illo itaque iste, tempore dolorum similique! Possimus provident molestias explicabo a fuga esse aperiam temporibus earum tenetur laborum quae quia expedita, labore distinctio quasi eveniet enim error perspiciatis impedit nihil illum porro. Soluta, quae?</p>
+    </div>
+  )
+}
---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+        <img src="spinner1.svg" alt="loading..." srcset="" className="h-96"/>
+    </div>
+  )
+}
