43b22bf6d9f3d53f1b61e18a94dddca0df9e186b
dineshbabu <dineshbabu1732@gmail.com>
2023-07-08 00:17:10 +0530

    add movie page

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -14,7 +14,7 @@ export default async function MoviePage({params}) {
 
   return (
     <div className="w-full">
-      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6"></div>
+      <div className="p-4 md:pt-8 flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6"></div>
       <Image
         src={`https://image.tmdb.org/t/p/original/${
           movie.backdrop_path || movie.poster_path
