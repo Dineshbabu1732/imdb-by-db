a269fd2693f1722a8da9ff8ce09d291a2bd3a7dd
dineshbabu <dineshbabu1732@gmail.com>
2023-07-07 23:31:13 +0530

    add card component

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,7 @@
 export default function loading() {
   return (
     <div className="flex justify-center">
-        <img src="spinner1.svg" alt="loading..." srcset="" className="h-96"/>
+        <img src="spinner.svg" alt="loading..." srcset="" className="h-96"/>
     </div>
   )
 }
